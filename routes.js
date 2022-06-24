0c1e613082605794c1339d9fe983d4fb23b9a425
Kamal <kamal.shaho@olivegroup.io>
2022-06-24 13:13:24 +0545

    signup

--- routes.js
+++ routes.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 router.get('/login', (req, res)=> {
     res.send('login user')
+});
+
+router.get('/signup', (req, res)=> {
+    res.send('signup user')
 })
 
 module.exports = router;
